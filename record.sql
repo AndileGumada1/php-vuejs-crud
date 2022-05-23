42111755cb2210056a2820517617d947338c08bc
andile <decemberandile@gmail.com>
2022-05-23 11:28:55 +0200

    first commit

---
+++ record.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `record` (
+  `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
